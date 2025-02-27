873d0d28ba122ea2a5e0c444b7f1621a658ec35b
Shashwat pandey <119878251+Aspirant001@users.noreply.github.com>
2025-02-27 05:15:43 +0000

    new

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // this uses all JS code as newer version
+
+// alert("hello") // this is used in browser console
+
+let name = "Shashwat"
+let age = 21
+let isLoggedIn = true
+
+// number datatype (range = 2^53)
+// bigint(same as long in java)
+// string datatype("")
+// boolean datatype
+// null = this itself is a standalone value/ representation of a empty value
+// undefined = value not defined after decalring a datatype
+// symbol (used mostly in react, figma, etc) to define individual component or uniqueness
+//object 
+
+console.log(typeof null); //null is an object in typeof(sort of language error)
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accoundId = 1219852
+let accountEmail = "sh@gmail.com"
+var accountPassword = "12121"
+accountCity = "Bhopal" //ye bhi var declare hogya hai but not advisable to use
+// we dont usually use var type. we use let. coz var mmein scope ka prblm hai, not in let
+
+console.table([accoundId, accountEmail, accountPassword, accountCity])
