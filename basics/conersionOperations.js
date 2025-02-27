959a544effedf5b68a8d780af208eb6cc9b6d5b4
Shashwat pandey <119878251+Aspirant001@users.noreply.github.com>
2025-02-27 05:37:43 +0000

    conversion

---
+++ basics/conersionOperations.js
@@ -0,0 +1,10 @@
+let score = true
+console.log(typeof score);
+
+let newScore = Number(score)
+console.log(typeof newScore)
+console.log(newScore)
+
+let log = 1
+let isLog = Boolean(log)
+console.log(isLog)
