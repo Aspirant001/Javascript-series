a6c0e0ff468a16197d2023cb5ade3b693e911f69
Shashwat pandey <119878251+Aspirant001@users.noreply.github.com>
2025-03-08 16:56:16 +0000

    new

--- basics/conersionOperations.js
+++ basics/conersionOperations.js
@@ -8,3 +8,21 @@ console.log(newScore)
 let log = 1
 let isLog = Boolean(log)
 console.log(isLog)
+
+//operations
+let value = 2
+let negVal = -value
+//2+2, 2-2, 2*2, 2**2(power), 2/2, etc
+
+let str1 = "Shashwat"
+let str2 = " Pandey"
+let str3 = str1+str2
+console.log(str3);
+
+
+console.log("1" + 2) //12 
+console.log("1" + 2) //12 
+console.log("1" + "2") //12
+console.log(1 + "2") //12
+console.log(1 + "2"+ "3") //123
+console.log(1 + 2+ "3") //33
